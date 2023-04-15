51cfd42614b11b0f532502438fde155ef96f8fb7
Janhvi Singh <janhvi52@gmail.com>
2023-04-16 03:26:48 +0530

    animation adde

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,62 +1,88 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Button } from "./Button";
 
-const Section = styled.section`
-width:100%;
-height:100%;
-padding:4rem 0rem;
+const slideFromLeft = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(-50px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
 `;
-const Container = styled.div`
-padding:3rem calc((100vw-1300px)/2);
-display:grid;
-grid-template-columns:1fr 1fr;
-grid-template-rows:800px;
 
-@media screen and (max-width:768px){
-    grid-template-columns:1fr;
-}`;
+const slideFromRight = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const Section = styled.section`
+  width: 100%;
+  height: 100%;
+  padding: 4rem 0rem;
+`;
+
+const Container = styled.div`
+  padding: 3rem calc((100vw - 1300px) / 2);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 800px;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
 
 const ColumnLeft = styled.div`
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-line-height:1.4;
-padding:1rem 2rem;
-order: $(({reverse})=>(reverse?'2':'1'));
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.4;
+  padding: 1rem 2rem;
+  order: ${({ reverse }) => (reverse ? "2" : "1")};
+  animation: ${slideFromLeft} 2s ease-in-out;
 
-h1{
-    margin-bottom:1rem;
-    font-size:clamp(1.5rem,6vw,2rem);
-}
+  h1 {
+    margin-bottom: 1rem;
+    font-size: clamp(1.5rem, 6vw, 2rem);
+  }
 
-p{
-    margin-bottom:2rem;
-}
+  p {
+    margin-bottom: 2rem;
+  }
 `;
 
 const ColumnRight = styled.div`
-padding:1rem 2rem;
-order: $(({reverse})=>(reverse?'1':'2'));
-display:flex;
-justify-content:center;
-align-items:center;
+  padding: 1rem 2rem;
+  order: ${({ reverse }) => (reverse ? "1" : "2")};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${slideFromRight} 1s ease-in-out;
 
-@media screen and (max-width:768px){
-    order: $(({reverse})=>(reverse?'2':'1'))
-}
+  @media screen and (max-width: 768px) {
+    order: ${({ reverse }) => (reverse ? "2" : "1")};
+  }
 
-img{
-    width:100%;
-    height:100%;
-    object-fit:cover;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 
-    @media screen and (max-width:768px){
-        width:90%;
-    height:90%;
+    @media screen and (max-width: 768px) {
+      width: 90%;
+      height: 90%;
     }
-}
+  }
 `;
 
 const InfoSection = ({
@@ -65,7 +91,7 @@ const InfoSection = ({
   paragraphTwo,
   buttonLabel,
   reverse,
-  image
+  image,
 }) => {
   return (
     <Section>
@@ -74,7 +100,9 @@ const InfoSection = ({
           <h1>{heading}</h1>
           <p>{paragraphOne}</p>
           <p>{paragraphTwo}</p>
-          <Button to="/homes" primary="true">{buttonLabel}</Button>
+          <Button to="/homes" primary="true">
+            {buttonLabel}
+          </Button>
         </ColumnLeft>
         <ColumnRight reverse={reverse}>
           <img src={image} alt="home" />
